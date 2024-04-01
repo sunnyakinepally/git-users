b8cceb01d55310a095df36b22e1c3bfc49d23e3a
sunny <sunnyakinepally1433@gmail.com>
2024-04-01 20:24:25 +0530

    Updated search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,14 @@ import { SearchComponent } from './search/search.component';
 import { HistoryComponent } from './history/history.component';
 import { HomeComponent } from './home/home.component';
 
+// // TO HOST THIS APP LOCALLY CHNAGE ROUTES TO LIKE THIS
+//  const routes: Routes = [
+// //   { path: '', component: HomeComponent },
+// //   { path: 'search', component: SearchComponent },
+// //   { path: 'History', component: HistoryComponent },
+
+// ];
+
 const routes: Routes = [
   { path: 'git-users', component: HomeComponent },
   { path: 'git-users/search', component: SearchComponent },
